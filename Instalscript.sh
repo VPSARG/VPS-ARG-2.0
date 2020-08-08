71f56303f239aa20f6c9f06864f9c2dcdc38a4b8
VPS-ARG <67613737+VPS-ARG@users.noreply.github.com>
2020-08-08 16:30:24 -0300

    Update Instalscript.sh

--- Instalscript.sh
+++ Instalscript.sh
@@ -23,8 +23,8 @@ AZUL='\e[34m' && MAGENTA='\e[35m' && MAG='\033[1;36m' &&NEGRITO='\e[1m' && SEMCO
 }
 clear
  msg -bar2
- msg -ama "     [ VPS-ARGENTO \033[1;97m 🔹Mod by @Pablo_Ezekiel🔹\033[1;33m ]"
- echo -e  "\033[1;97m              EJECUTANDO -  ACTUALIZADORES  \033[1;34m "
+ msg -ama "     [ VPS-ARGENTO \033[1;34m 🔹by @Pablo_Ezekiel🔹\033[1;33m ]"
+ echo -e  "\033[5;34m              EJECUTANDO -  ACTUALIZADORES  \033[1;34m "
  msg -bar2
 ## Script name
 SCRIPT_NAME=vpsargup
@@ -36,20 +36,20 @@ INSTALL_DIR=${INSTALL_DIR_PARENT}${SCRIPT_NAME}/
 mkdir -p "/etc/vpsargup/"
 ## Install/update
 if [ ! -d "$INSTALL_DIR" ]; then
-	echo -e  "\033[1;97m           Instalando Paquetes Especiales"
+	echo -e  "\033[5;34m           Paquetes Esenciales"
 	echo "          ================"
 	sleep 2
 	mkdir -p "$INSTALL_DIR_PARENT"
 	cd "$INSTALL_DIR_PARENT"
-    wget https://raw.githubusercontent.com/VPSARG/VPS-ARG-2.0/master/Zzupdate-master/zzupdate-default.conf?token=AQD3IKKYEUEQ2MIXQNZXAYS7F3W3M -O /usr/local/vpsargup/vpsargup.default.conf  &> /dev/null
+    wget https://raw.githubusercontent.com/VPSARG/VPS-ARG-2.0/master/Zzupdate-master/zzupdate-default.conf -O /usr/local/vpsargup/vpsargup.default.conf  &> /dev/null
 	#chmod +x /usr/local/vpsargup/vpsargup.default.conf 
 	rm -rf /usr/local/vpsargup/vpsargup.sh
-    wget https://raw.githubusercontent.com/VPSARG/VPS-ARG-2.0/master/Zzupdate-master/zzupdate.sh?token=AQD3IKPD647HK5P7DGAN3C27F3W5U -O /usr/local/vpsargup/vpsargup.sh &> /dev/null
+    wget https://raw.githubusercontent.com/VPSARG/VPS-ARG-2.0/master/Zzupdate-master/zzupdate.sh -O /usr/local/vpsargup/vpsargup.sh &> /dev/null
 	chmod +x /usr/local/vpsargup/vpsargup.sh
 	rm -rf /usr/bin/vpsargup
-    wget https://raw.githubusercontent.com/VPSARG/VPS-ARG-2.0/master/Zzupdate-master/zzupdate.sh?token=AQD3IKPD647HK5P7DGAN3C27F3W5U -O /usr/bin/vpsargup &> /dev/null
+    wget https://raw.githubusercontent.com/VPSARG/VPS-ARG-2.0/master/Zzupdate-master/zzupdate.sh -O /usr/bin/vpsargup &> /dev/null
 	chmod +x /usr/bin/vpsargup
-	echo -e  "\033[1;97m               Instalador Interno "
+	echo -e  "\033[5;97m               Instalador Interno "
 	
 	echo "           --------------------------------"	
 	
@@ -60,29 +60,29 @@ else
 fi
 
 ubu16_fun () {
-    wget -O /etc/apt/sources.list https://raw.githubusercontent.com/VPSARG/VPS-ARG-2.0/master/Repositorios/16.04/sources.list?token=AQD3IKO475HZ2RZR2L5PHPS7F3XUS &> /dev/null
-	echo -e "\033[1;97m SELECCIONO “ UBUNTU 16"
+    wget -O /etc/apt/sources.list https://raw.githubusercontent.com/VPSARG/VPS-ARG-2.0/master/Repositorios/16.04/sources.list &> /dev/null
+	echo -e "\033[3;97m SELECCIONO “ UBUNTU 16"
 }
 
 ubu18_fun () {
-    wget -O /etc/apt/sources.list https://raw.githubusercontent.com/VPSARG/VPS-ARG-2.0/master/Repositorios/18.04/sources.list?token=AQD3IKOBAON26PRVTX33NYS7F3XXG &> /dev/null
-	echo -e "\033[1;97m SELECCIONO “ UBUNTU 18"
+    wget -O /etc/apt/sources.list https://raw.githubusercontent.com/VPSARG/VPS-ARG-2.0/master/Repositorios/18.04/sources.list &> /dev/null
+	echo -e "\033[3;97m SELECCIONO “ UBUNTU 18"
 }	
 
 otro_fun () {
     
 	echo "OK SELECCIONO “ OTRO"
 }
-	echo -e "\033[1;97m           --¿QUE UBUNTU ESTA UTILIZANDO?--"
-	echo -e "\033[1;97m  Digite solo el numero segun respuesta: "
+	echo -e "\033[1;3;34m           --¿QUE UBUNTU ESTA UTILIZANDO?--"
+	echo -e "\033[4;34m  Digite el numero segun respuesta: "
     msg -bar
-    echo -e "\033[1;97m Escoja la opcion deseada."
+    echo -e "\033[1;34m Escoja la opcion deseada."
     msg -bar
     echo " 1).- Ubuntu 16.04 "
     echo " 2).- Ubuntu 18.04 "
     echo " 3).- Otro "
 	msg -bar
-	echo -n "Digite solo el numero segun respuesta: "
+	echo -n "Digite el numero segun respuesta: "
     read opcao
     case $opcao in
     1)
